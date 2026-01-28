fa0fb39862039fa780b181668d49b66aab0728e9
Daniel G. Pinto <danielpintolzr@gmail.com>
2026-01-27 23:01:49 -0500

    ram.sv created for IMEM and DMEM

--- src/register.sv
+++ src/register.sv
@@ -1,11 +1,11 @@
-module register(//32-bit register
+module register( //32-bit register
     input logic clk, 
     input logic reset, 
     input logic enable, 
     input logic in[31:0], 
     output logic out[31:0]
 )
-    always_ff (@posedge clk) begin
+    always_ff @(posedge clk or posedge reset) begin
         if (reset)
             out<=32'b0; 
         else if (enable)
