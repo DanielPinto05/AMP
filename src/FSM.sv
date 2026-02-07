23a6af57a0b50c84fa3ffbe9492f2199b3f0537f
Daniel G. Pinto <danielpintolzr@gmail.com>
2026-02-07 15:12:41 -0500

    FSM next state logic preliminary implementation

--- src/FSM.sv
+++ src/FSM.sv
@@ -34,25 +34,40 @@ else
 end
 
 /*compute next state based on current + opcodes*/
+//! note that ONLY lw/sw is implemented for now. Next step is lb, lh, lhu
 always_comb begin
-    next_state = state; //* hold state if 
+    next_state = state; //* hold state if next_state doesn't get assigned
     case(state)
         RESET: next_state = FETCH; 
         FETCH: next_state = DECODE; 
         DECODE: 
             begin unique casez(op)
-                3: next_state = MEMADR//lw
-                35: next_state = 
-                default: next_state = RESET
-
+                3: next_state = MEMADR; //lw
+                35: next_state = MEMADR; //sw
+                default: next_state = RESET;
+                51: next_state = EXECUTER; //R-type
+                19: next_state = EXECUTEI; //I-tpye
+                99: next_state = BRANCH; 
+                111: next_state = JAL; 
+                23: next_state = LUI; 
             endcase
-
-
             end
-        MEMADR:
-
-
-
+        MEMADR: begin
+            if (op==3)
+                next_state = MEMREAD; // lw
+            else if (op==35)
+                next_state = MEMWRITE; // sw
+            end
+        MEMREAD: next_state = MEMWB; // writeback to registers
+        MEMWB: next_state = FETCH; 
+        MEMWRITE: next_state = FETCH; 
+        EXECUTER: next_state = ALUWB; 
+        EXECUTEI: next_state = ALUWB; 
+        ALUWB: next_state = FETCH; 
+        BRANCH: next_state = FETCH; 
+        JAL: next_state = ALUWB; 
+        LUI: next_state = ALUWB; 
+        default: next_state = RESET; 
     endcase
 end
 
