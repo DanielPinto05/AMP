9e71bf822a66c4222e1820be4943e6db9aa898d2
Fadi <fadi.joseph100@yahoo.com>
2026-02-07 14:22:20 -0500

    Added mux files.

---
+++ src/mux.sv
@@ -0,0 +1,29 @@
+module 2to1mux (input logic mx,
+                input logic [31:0] a,
+                input logic [31:0] b,
+                output logic [31:0] out);
+    always_comb begin
+        case(mx)
+            0: out = a;
+            1: out = b;
+            default: 0;
+        endcase
+    end
+endmodule
+
+module 4to1mux (input logic [1:0] mx,
+                input logic [31:0] a,
+                input logic [31:0] b,
+                input logic [31:0] c,
+                input logic [31:0] d,
+                output logic [31:0] out);
+     always_comb begin 
+        case(mx)
+            0: out = a;
+            1: out = b;
+            2: out = c;
+            3: out = d;
+            default: 0;
+        endcase
+     end
+endmodule
