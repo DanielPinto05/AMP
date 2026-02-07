1e83af06a088f297ffaedb75277057fadf3d78fd
Fadi <fadi.joseph100@yahoo.com>
2026-02-07 15:15:10 -0500

    datapath in progress.

--- src/amp.sv
+++ src/amp.sv
@@ -1,22 +1,119 @@
-module amp() 
-// top-level module
-logic clk; 
-logic reset; 
-logic PCwrite; 
+module amp (input logic clk, 
+            input logic reset,
+            output logic [31:0] result); 
+    // top-level module
+   
+    // PC register
+    logic [31:0] PCNext;
+    logic [31:0] PCout;
+    register pc(
+        .clk(clk), 
+        .reset(reset), 
+        .enable(PCwrite),
+        .in(PCNext),
+        .out(PCout)
+    )
+
+    logic [31:0] Adr;
+    2to1mux pc_2to1_mux(
+        .mx(AdrSrc),
+        .a(PCout),
+        .b(Result),
+        .out(Adr)
+    );
+    
+    // Instruction Memory
+    logic [31:0] Instr;
+    ram IMEM() // instruction memory - this is read only
 
 
+    // Data Memory
+    ram DMEM() // data memory
 
+    // Register File
+    logic [31:0] rd1;
+    logic [31:0] rd2;
+    logic [31:0] A;
+    logic [31:0] WriteData;
+    
+    register_file u_register_file(
+        .clk(clk),
+        .reset(reset),
+        .RegWrite(RegWrite),
+        .rs1(Instr[19:15]),
+        .rs2(Instr[24:20]),
+        .rd(Instr{11:7}),
+        .wd(Result),
+        .rd1(rd1),
+        .rd2(rd2),
+    );  
 
-register pc(
-    .clk(clk), 
-    .reset(reset), 
-    .enable(PCwrite), 
-    .)
+    register rd1_register(
+        .clk(clk), 
+        .reset(reset), 
+        .enable(1'b1),
+        .in(rd1),
+        .out(A)
+    )
 
+     register rd2_register(
+        .clk(clk), 
+        .reset(reset), 
+        .enable(1'b1),
+        .in(rd2),
+        .out(WriteData)
+    )
 
-ram IMEM() // instruction memory - this is read only
+    // Src A Mux
+    logic [31:0] SrcA;
 
-ram DMEM() // data memory
+    // Src B Mux
+    logic [31:0] SrcB;
 
+    // ALU
+    logic [31:0] ALUResult;
+    ALU alu_instance(
+        .a(SrcA),
+        .b(SrcB),
+        .ALUctrl(ALUControl),
+        .ALUout(ALUResult)
+    );
+
+    // Result
+    logic [31:0] ALUOut;
+    logic [31:0] Result;
+    register u_reg (
+        .clk(clk),
+        .reset(reset),
+        .RegWrite(1'b1),
+        .in(ALUResult),
+        .out(ALUOut)
+    );
+
+    4to1mux result_mux (
+        .mx(ResultSrc)
+        .a(ALUOut)
+        .b(Data)
+        .c(ALUResult)
+        .d(32'd0)
+        .out(Result)
+    );
+
+    // Control Unit FSM
+    logic PCwrite; 
+    logic AdrSrc;
+    logic MemWrite;
+    logic IRWrite;
+    logic [1:0] ResultSrc;
+    logic [3:0] ALUControl;
+    logic [1:0] ALUSrcB;
+    logic [1:0] AlUSrcA;
+    logic [1:0] ImmSrc;
+    logic RegWrite;
+
+    FSM fsm_instance(
+        
+
+    )
 
 endmodule
--- src/mux.sv
+++ src/mux.sv
@@ -1,29 +1,33 @@
-module 2to1mux (input logic mx,
-                input logic [31:0] a,
-                input logic [31:0] b,
-                output logic [31:0] out);
+module 2to1mux (
+    input logic mx,
+    input logic [31:0] a,
+    input logic [31:0] b,
+    output logic [31:0] out
+);
     always_comb begin
         case(mx)
             0: out = a;
             1: out = b;
-            default: 0;
+            default: out = 0;
         endcase
     end
 endmodule
 
-module 4to1mux (input logic [1:0] mx,
-                input logic [31:0] a,
-                input logic [31:0] b,
-                input logic [31:0] c,
-                input logic [31:0] d,
-                output logic [31:0] out);
-     always_comb begin 
+module 4to1mux (
+    input logic [1:0] mx,
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input logic [31:0] c,
+    input logic [31:0] d,
+    output logic [31:0] out
+);
+    always_comb begin 
         case(mx)
             0: out = a;
             1: out = b;
             2: out = c;
             3: out = d;
-            default: 0;
+            default: out = 0;
         endcase
-     end
+    end
 endmodule
